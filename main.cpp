eb4e905e7d8041e5d759c6360feb06159fe93297
Dermot Logue <dermot.logue@dkit.ie>
2025-01-26 22:29:11 +0000

    updated variable names and comments

--- main.cpp
+++ main.cpp
@@ -1,63 +1,62 @@
-#include <iostream>
+#include <iostream>         // Jan 2025
 #include <fstream> // used for access to the file input and output classes, ifstream and ofstream
 
+/*
+ * Demonstrates reading text from a file:
+ * 1. word by word
+ * 2. line by line
+ */
 using namespace std;
 
 // function prototypes
-void read_word_by_word(ifstream&);  // note the reference type "&"
-void read_line_by_line(ifstream&);
+void read_word_by_word(ifstream &);  // note the reference type "&"
+void read_line_by_line(ifstream &);
 
 int main() {
 
-    ifstream fin("input.txt");  // creates an input file stream to the file "input.txt"
-    if(fin)                     // ensures the file input stream is opened correctly
-    {
+    ifstream ifs("input.txt");   // creates an input file stream object (ifs) based on the file "input.txt"
+
+    if (ifs) {                        // if "ifs" evaluates as "true", then the file input stream has opened successfully
         cout << "\nWord at a time:" << endl;
-        read_word_by_word( fin );// creates a variable to store each word we read in from the file.
+        read_word_by_word(ifs);   // call a function to read words from the stream and print them
+        // the input file stream "ifs" is passed by reference
+    } else {
+        cout << "Unable to open file" << endl;
     }
-    else
-    {
-        cout << "Unable to open file" <<endl;
-    }
-    fin.close();    // close the file
+    ifs.close();    // close the file
 
-    fin.open("input.txt");      // open the file again
-    // now read file line by line
-   if(fin)                     // ensures the file input stream is opened correctly
-    {
-       cout << "\nLine at a time:" << endl;
-        read_line_by_line( fin );// creates a variable to store each word we read in from the file.
-    }
-    else
-    {
-        cout << "Unable to open file" <<endl;
-    }
-    fin.close();    // close the file
+    ifs.open("input.txt");      // open the file again, and starts at beginning
 
+    if (ifs) {                    //if file has opened successfully
+        cout << "\nLine at a time:" << endl;
+        read_line_by_line(ifs);// pass the ifs into a function that will read its content line-by-line
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+    ifs.close();    // close the file
 
     return 0;
 }
 
-void read_word_by_word(ifstream & ifstream1 ) {
-    string aWord;
-    while(!ifstream1.eof())     // while not at end of file
-    {
-        ifstream1 >> aWord;     // read a word from the stream into variable
-        cout << aWord << " ";   // print each word to the screen followed by a space
+/**
+ * Reads words from the input stream, one at a time, and outputs each one.
+ * The words are delimited (seperated) by whitespace (i.e. spaces or tabs)
+ *
+ * @param ifs - reference to an Input File Stream
+ */
+void read_word_by_word(ifstream &ifs) {  // ifs is a reference to an input file stream
+    string word;
+    while (!ifs.eof()) {        // while not at the end of the file
+        ifs >> word;            // extract a word from the stream into the variable 'word'(as a string)
+        cout << word << " ";    // print out the word to the screen followed by a space
     }
 }
 
-void read_line_by_line(ifstream & ifstream1 ) {
+void read_line_by_line(ifstream &ifstream1) {
     string line;
-    while( getline(ifstream1,line) )     // while not at end of file, read a line
+    while (getline(ifstream1, line))     // while not at end of file, read a line
     {
         cout << line << " ";   // print each word to the screen followed by a space
     }
 }
 
-/*
- *
- * 1. Modify the above code to read in the data line-by-line instead of word-by-word.
- *
- * 2. Use a function to read in the data from the file and return it as a string.
- */
